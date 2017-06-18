4f1e4f89f2175ed1ac0412baa2c5c9fad6f4c92a
Andrew Collington <andy@amnuts.com>
2017-06-18 14:53:24 +0100

    Added threading to flash an LED without interrupting main thread, and added config entry for the path

--- recorder.py
+++ recorder.py
@@ -1,5 +1,6 @@
 import json
 import os
+import threading
 import time
 import picamera
 import RPi.GPIO as GPIO
@@ -17,21 +18,40 @@ def button_pushed():
         running = False
 
 
+def flash_led(times, long_end=False):
+    global config
+    for x in range(times):
+        if config['debug']:
+            print('Flash LED %d' % filename)
+        GPIO.output(config['led_pin'], 1)
+        time.sleep(0.5)
+        GPIO.output(config['led_pin'], 0)
+    if long_end:
+        GPIO.output(config['led_pin'], 1)
+        time.sleep(2)
+        GPIO.output(config['led_pin'], 0)
+
+
 with open(os.path.dirname(os.path.realpath(__file__)) + '/config.json', 'r') as f:
     config = json.load(f)
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(config['led_pin'], GPIO.OUT)
 GPIO.setup(config['button_pin'], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.add_event_detect(config['button_pin'], GPIO.RISING, callback=button_pushed, bouncetime=200)
 
 try:
+    t = threading.Thread(target=flash_led, args=[3, True])
+    t.start()
     while True:
         with picamera.PiCamera() as camera:
             while running:
-                for filename in camera.capture_continuous('~/timelapse-%04d/img{counter:04d}.jpg' % lapse):
+                for filename in camera.capture_continuous(config['save_path'] % lapse):
                     if config['debug']:
                         print('Captured %s' % filename)
                     time.sleep(config['sleep_seconds'])
+                    t = threading.Thread(target=flash_led, args=[1])
+                    t.start()
                     if not running:
                         break
 finally:
